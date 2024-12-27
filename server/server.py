2a999866892147154153149602e585d5149e7dc6
Rosbarfi <rosbarfi05@gmail.com>
2024-12-28 00:09:31 +0530

    updated files with changes-13

--- server/server.py
+++ server/server.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify,render_template #render template
 from . import util 
 import streamlit
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def index():
